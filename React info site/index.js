7ea9be27e7945f073cb28e81aeb23ff513f7cdc9
Corentin <corentin.chat@gmail.com>
2023-01-29 19:42:18 +0100

    refactor create our first component

--- React info site/index.js
+++ React info site/index.js
@@ -1,15 +1,17 @@
-const page = (
-<div>
-    <img src="https://miro.medium.com/fit/c/184/184/1*K0a7xINk0RM5gfXGSN68cw.png" width="40px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-</div>
-)
+function Page(){
+    return (
+        <div>
+            <img src="https://miro.medium.com/fit/c/184/184/1*K0a7xINk0RM5gfXGSN68cw.png" width="40px" />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
