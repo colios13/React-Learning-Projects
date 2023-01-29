6e6e697ba5b334dabef589b6512ce91f47eb91d2
Corentin <corentin.chat@gmail.com>
2023-01-29 18:48:09 +0100

    feat insert react with cdn  and add some text

---
+++ React info site/index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+<div>
+    <h1>Learning React</h1>
+    <ul>
+        <li>That's my first element</li>
+        <li>That's my second element</li>
+    </ul>
+</div>, 
+document.getElementById("root"))
