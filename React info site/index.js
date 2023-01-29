fc74d1e88eefca5491590b22cf0a5852fb35d3c3
Corentin <corentin.chat@gmail.com>
2023-01-29 19:07:01 +0100

    refactor change the page text to match our goal app

--- React info site/index.js
+++ React info site/index.js
@@ -1,9 +1,15 @@
-ReactDOM.render(
+const page = (
 <div>
-    <h1>Learning React</h1>
+    <img src="https://miro.medium.com/fit/c/184/184/1*K0a7xINk0RM5gfXGSN68cw.png" width="40px" />
+    <h1>Fun facts about React</h1>
     <ul>
-        <li>That's my first element</li>
-        <li>That's my second element</li>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
-</div>, 
-document.getElementById("root"))
+</div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
