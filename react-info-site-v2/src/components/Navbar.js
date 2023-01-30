9667d949a82c366eb9001e860de8cf6f07e17262
Corentin <corentin.chat@gmail.com>
2023-01-30 12:44:02 +0100

    style: add style to the navbar

--- react-info-site-v2/src/components/Navbar.js
+++ react-info-site-v2/src/components/Navbar.js
@@ -1,5 +1,11 @@
 function Navbar() {
-    return ( <h1>Navbar goes here</h1> );
+    return ( 
+        <nav>
+            <img className="nav--icon" alt="react logo" src="https://miro.medium.com/fit/c/184/184/1*K0a7xINk0RM5gfXGSN68cw.png"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    );
 }
 
 export default Navbar;
