3389ea8455fdf1ba612ab90c272f983f6345cd59
Corentin <corentin.chat@gmail.com>
2023-02-04 22:37:30 +0100

    feat: add the navbar content

---
+++ desired-travel-places/src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return ( 
+        <nav>
+            <img alt="earth icon" />
+            <h3>my desired travel places</h3>
+        </nav>
+     );
+}
+
+export default Navbar;
