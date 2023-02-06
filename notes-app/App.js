b5045c9b9dc321ac6396d8cb424e110ed1ddc11f
Corentin <corentin.chat@gmail.com>
2023-02-06 23:20:06 +0100

    feat: store and access the notes in local storage

--- notes-app/App.js
+++ notes-app/App.js
@@ -6,7 +6,7 @@ import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || [])
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
@@ -17,15 +17,17 @@ export default function App() {
             body: "# Type your markdown note's title here"
         }
         setNotes(prevNotes => [newNote, ...prevNotes])
+        localStorage.setItem("notes",JSON.stringify(notes))
         setCurrentNoteId(newNote.id)
     }
     
     function updateNote(text) {
         setNotes(oldNotes => oldNotes.map(oldNote => {
             return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
+                ? { id: oldNote.id, body: text }
                 : oldNote
         }))
+        localStorage.setItem("notes",JSON.stringify(notes))
     }
     
     function findCurrentNote() {
