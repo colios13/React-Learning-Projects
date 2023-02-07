d9b3b729632654bc9e96f4c258617befc48f4e4a
Corentin <corentin.chat@gmail.com>
2023-02-07 14:39:34 +0100

    feat: create the dice component and display dices

---
+++ tenzies/src/components/Dice.js
@@ -0,0 +1,9 @@
+function Dice(props) {
+    return ( 
+    <div className="dice">
+        <span className="dice--number">{props.number}</span>
+    </div>
+    );
+}
+
+export default Dice;
--- tenzies/src/components/Main.js
+++ tenzies/src/components/Main.js
@@ -1,9 +1,24 @@
-
+import Dice from "./Dice";
 
 function Main() {
+    let allDice = []
+    for (let i = 0; i < 2; i++) {
+        let inlineDice = []
+        for (let j = 0; j < 10; j++) {
+            inlineDice.push(<Dice key={i*10+j} number={Math.floor(Math.random()*6)} />)
+        }
+        allDice.push(inlineDice)
+    }
 
     return ( 
         <main className="main">
+            <h2>Tenzies game</h2>
+            <section className="main--dice-container">
+                <div className="main--inline-dice">
+                    {allDice[0]}
+                </div>
+                <div className="main--inline-dice">{allDice[1]}</div>
+            </section>
         </main>
      );
 }
