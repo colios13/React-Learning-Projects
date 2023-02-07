a76e4a3a4bfff050e441522b03224ca57ad7ba9c
Corentin <corentin.chat@gmail.com>
2023-02-07 16:22:54 +0100

    style: dice container and dice

--- tenzies/src/components/Main.js
+++ tenzies/src/components/Main.js
@@ -2,23 +2,18 @@ import Dice from "./Dice";
 
 function Main() {
     let allDice = []
-    for (let i = 0; i < 2; i++) {
-        let inlineDice = []
-        for (let j = 0; j < 10; j++) {
-            inlineDice.push(<Dice key={i*10+j} number={Math.floor(Math.random()*6)} />)
-        }
-        allDice.push(inlineDice)
+    for (let j = 0; j < 10; j++) {
+        allDice.push(<Dice key={j} number={Math.floor(Math.random()*6)} />)
     }
 
     return ( 
         <main className="main">
-            <h2>Tenzies game</h2>
-            <section className="main--dice-container">
-                <div className="main--inline-dice">
-                    {allDice[0]}
-                </div>
-                <div className="main--inline-dice">{allDice[1]}</div>
-            </section>
+            <div className="main--content">
+                <h2 className="main--title">Tenzies</h2>
+                <section className="main--dice-container">
+                    {allDice}
+                </section>
+            </div>
         </main>
      );
 }
