1e3de033602e23200a6a960fc9e04aa9386f0a5a
Corentin <corentin.chat@gmail.com>
2023-02-21 16:58:07 +0100

    style: adjust container size and add images

--- vite-styled-components/src/components/Header.jsx
+++ vite-styled-components/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Container } from "./styles/Container.styled";
 import { Logo, Nav, StyledHeader } from "./styles/Header.styled";
+import logo from "../images/logo.svg"
 import { Button } from "./styles/Button.styled";
 
 
@@ -8,7 +9,7 @@ function Header() {
     <StyledHeader>
       <Container>
         <Nav>
-          <Logo src='' alt='chat icon and Huddle write to it right' />
+          <Logo src={logo} alt='chat icon and Huddle write to it right' />
           <Button>Try it Free</Button>
         </Nav>
       </Container>
