8b7f31b80f4ec65b4ffec5662ea47af27773a923
Corentin <corentin.chat@gmail.com>
2023-02-21 10:52:47 +0100

    feat: add the error overlay for runtime error

--- vite-styled-components/src/main.jsx
+++ vite-styled-components/src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// REGISTER ERROR OVERLAY
+const showErrorOverlay = err => {
+  // must be within function call because that's when the element is defined for sure.
+  const ErrorOverlay = customElements.get('vite-error-overlay')
+  // don't open outside vite environment
+  if (!ErrorOverlay) {return}
+  console.log(err)
+  const overlay = new ErrorOverlay(err)
+  document.body.appendChild(overlay)
+}
+
+window.addEventListener('error', showErrorOverlay)
+window.addEventListener('unhandledrejection', ({reason}) => showErrorOverlay(reason))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
